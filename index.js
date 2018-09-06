194dc65e70dea3249ac011fe2901f00c8056020f
devinkapelanski@yahoo.com <devinkapelanski@yahoo.com>
2018-09-06 22:57:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,15 +2,12 @@
 function happyHolidays(string) {
   return "Happy holidays!"
 }
-
 function happyHolidaysTo(name) {
-  return "Happy holidays, ${name}!"
+  return `Happy holidays, ${name}!`
 }
-
-function happyHolidayTo(holiday, name) {
-  return "Happy ${holiday}, ${name}!"
+ function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
 }
-
-function holidayCountdown(days, holiday) {
-  return "It\'s ${days} days until ${holiday}!"
+ function holidayCountdown(holiday, days) {
+  return `It\'s ${days} days until ${holiday}!`
 }
